c5870cf6423690370292e375ed3dc21f4f2f50c3
Cameron Brandenburg <CPBrandenburg@gmail.com>
2020-04-27 00:13:57 -0400

    Added Assignment 15

---
+++ csce242/assignments/a15/public/script.js
@@ -0,0 +1,210 @@
+async function displayRestaurants(){
+    let response = await fetch('api/restaurants/');
+    let restaurantsJSON = await response.json();
+    let restaurantsDiv = document.getElementById("restaurants-list");
+    restaurantsDiv.innerHTML = "";
+
+    for(i in restaurantsJSON){
+        let restaurant = restaurantsJSON[i];
+        restaurantsDiv.append(getRestaurantItem(restaurant));
+    }
+}
+
+function getRestaurantItem(restaurant){
+    let restaurantSection = document.createElement("section");
+    restaurantSection.classList.add("restaurant");
+
+    let aTitle = document.createElement("a");
+    aTitle.setAttribute("data-id", restaurant._id);
+    aTitle.href = "#";
+    aTitle.onclick = showRestaurantDetails;
+    let h3Elem = document.createElement("h3");
+    h3Elem.textContent = restaurant.name;
+    aTitle.append(h3Elem);
+    restaurantSection.append(aTitle);
+
+    return restaurantSection;
+}
+
+async function showRestaurantDetails(){
+    let resInfo = document.getElementById("restaurant-info");
+    resInfo.style.display = "flex";
+
+    let id = this.getAttribute("data-id");
+    let response = await fetch(`/api/restaurants/${id}`);
+
+    if(response.status != 200){
+        //display an error
+        console.log("Error reciving restaurant");
+        return;
+    }
+
+    let restaurant = await response.json();
+    document.getElementById("restaurant-id").textContent = restaurant._id;
+    document.getElementById("txt-name").value = restaurant.name;
+    document.getElementById("txt-website").value = restaurant.website;
+    document.getElementById("txt-address").value = restaurant.address;
+    document.getElementById("txt-price-range").value = restaurant.priceRange;
+    document.getElementById("txt-specialties").value = restaurant.specialties;
+    document.getElementById("txt-rating").value = restaurant.rating;
+    document.getElementById("txt-hours").value = restaurant.hours;
+
+    document.getElementById("p-name").textContent = restaurant.name;
+    document.getElementById("a-website").textContent = `Visit Website`;
+    document.getElementById("a-website").href = `#`;
+    document.getElementById("p-address").textContent = `Located at: ${restaurant.address}`;
+    document.getElementById("p-price-range").textContent = `Price range: ${restaurant.priceRange}`;
+    document.getElementById("p-specialties").textContent = `Known for: ${restaurant.specialties}`;
+    document.getElementById("p-rating").textContent = `Rating: ${restaurant.rating} / 5 stars`;
+    document.getElementById("p-hours").textContent = `Hours: ${restaurant.hours}`;
+}
+
+async function addRestaurant(){
+    let restaurantName = document.getElementById("txt-add-name").value;
+    let restaurantWebsite = document.getElementById("txt-add-website").value;
+    let restaurantAddress = document.getElementById("txt-add-address").value;
+    let restaurantPriceRange = document.getElementById("txt-add-price-range").value;
+    let restaurantSpecialties = document.getElementById("txt-add-specialties").value.split(",");
+    let restaurantRating = document.getElementById("txt-add-rating").value;
+    let restaurantHours = document.getElementById("txt-add-hours").value;
+
+    let restaurant = {
+        "name":restaurantName,
+        "website":restaurantWebsite,
+        "address":restaurantAddress,
+        "priceRange":restaurantPriceRange,
+        "specialties":restaurantSpecialties,
+        "rating":restaurantRating,
+        "hours":restaurantHours
+    };
+    
+    let response = await fetch('/api/restaurants',{
+        method:"POST",
+        headers:{
+            'Content-Type':'application/json;charset=utf-8',
+        },
+        body:JSON.stringify(restaurant),
+    });
+
+    if(response.status != 200){
+        console.log("ERROR posting data");
+        return;
+    }
+
+    let result = await response.json();
+    console.log(result);
+    displayRestaurants();
+
+    // Flushing the input fields after use
+    document.getElementById("txt-add-name").value = "";
+    document.getElementById("txt-add-website").value = "";
+    document.getElementById("txt-add-address").value = "";
+    document.getElementById("txt-add-price-range").value = "";
+    document.getElementById("txt-add-specialties").value = "";
+    document.getElementById("txt-add-rating").value = "";
+    document.getElementById("txt-add-hours").value = "";
+
+    // Hids the add new restaurant menu after use
+    toggleAdd();
+}
+
+async function editRestaurant(){
+    let restaurantId = document.getElementById("restaurant-id").textContent;
+    let restaurantName = document.getElementById("txt-name").value;
+    let restaurantWebsite = document.getElementById("txt-website").value;
+    let restaurantAddress = document.getElementById("txt-address").value;
+    let restaurantPriceRange = document.getElementById("txt-price-range").value;
+    let restaurantSpecialties = document.getElementById("txt-specialties").value.split(",");
+    let restaurantRating = document.getElementById("txt-rating").value;
+    let restaurantHours = document.getElementById("txt-hours").value;
+    
+    let restaurant = {
+        "name":restaurantName,
+        "website":restaurantWebsite,
+        "address":restaurantAddress,
+        "priceRange":restaurantPriceRange,
+        "specialties":restaurantSpecialties,
+        "rating":restaurantRating,
+        "hours":restaurantHours
+    };
+
+    let response = await fetch(`/api/restaurants/${restaurantId}`,{
+        method:'PUT',
+        headers:{
+            'Content-Type':'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(restaurant)
+    });
+
+    if(response.status != 200){
+        console.log("Error updating restaurant");
+        return;
+    }
+
+    let result = await response.json();
+    displayRestaurants();
+}
+
+async function deleteRestaurant(){
+    let resInfo = document.getElementById("restaurant-info");
+    resInfo.style.display = "none";
+
+    let restaurantId = document.getElementById("restaurant-id").textContent;
+    
+    let response = await fetch(`/api/restaurants/${restaurantId}`,{
+        method:"DELETE",
+        headers:{
+            'Content-Type':'application/json;charset=utf-8',
+        }
+    });
+
+    if(response.status != 200){
+        console.log("Error deleting");
+        return;
+    }
+
+    let result = await response.json();
+    displayRestaurants();
+}
+
+function toggleAdd() {
+    let addDiv = document.getElementById("add-restaurant");
+    if (addDiv.style.display === "none") {
+        addDiv.style.display = "block";
+    } else {
+        addDiv.style.display = "none";
+    }
+}
+
+function showEdit() {
+    let submitBtn = document.getElementById("btn-submit-restaurant");
+    submitBtn.classList.toggle("hidden");
+
+    let displayInfo = document.getElementById("display-info");
+    displayInfo.classList.toggle("hidden");
+    
+    let editInfo = document.getElementById("edit-info");
+    editInfo.classList.toggle("hidden");
+}
+
+
+
+window.onload = function(){
+    this.displayRestaurants();
+
+    let addBtn = document.getElementById("btn-add-restaurant");
+    addBtn.onclick = addRestaurant;
+
+    let editBtn = document.getElementById("btn-edit-restaurant");
+    editBtn.onclick = showEdit;
+
+    let submitBtn = document.getElementById("btn-submit-restaurant");
+    submitBtn.onclick = editRestaurant;
+
+    let deleteBtn = document.getElementById("btn-delete-restaurant");
+    deleteBtn.onclick = deleteRestaurant;
+
+    let btnAdd = document.getElementById("btn-add-new-restaurant");
+    toggleAdd();
+    btnAdd.onclick = toggleAdd;
+}
---
+++ csce242/assignments/a15/server.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const app = express();
+const Joi = require("joi");
+app.use(express.static("public"));
+app.use(express.json());
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost/restaurants", {useUnifiedTopology:true, useNewUrlParser:true})
+    .then(()=>console.log("Connected to mongodb"))
+    .catch(err => console.error("couldn't connect to mongdb", err));
+
+const restaurantSchema = new mongoose.Schema({
+    name:String,
+    website:String,
+    address:String,
+    priceRange:String,
+    specialties:[String],
+    rating:Number,
+    hours:String
+});
+
+const Restaurant = mongoose.model('Restaurant',restaurantSchema);
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get('/api/restaurants', (req,res)=>{
+    getRestaurants(res);
+});
+
+async function getRestaurants(res){
+    const restaurants = await Restaurant.find();
+    console.log(restaurants);
+    res.send(restaurants);
+}
+
+app.get('/api/restaurants/:id',(req,res)=>{
+    getRestaurant(req.params.id, res);
+});
+
+async function getRestaurant(id, res){
+    const restaurant = await Restaurant.findOne({_id:id});
+    console.log(restaurant);
+    res.send(restaurant);
+}
+
+app.post('/api/restaurants', (req,res)=>{
+    const result = validateRestaurant(req.body);
+    
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    
+    const restaurant = new Restaurant({
+        name:req.body.name,
+        website:req.body.website,
+        address:req.body.address,
+        priceRange:req.body.priceRange,
+        specialties:req.body.specialties,
+        rating:req.body.rating,
+        hours:req.body.hours
+    });
+
+    createRestaurant(restaurant,res);
+});
+
+async function createRestaurant(restaurant, res){
+    const result = await restaurant.save();
+    console.log(result);
+    res.send(restaurant);
+}
+
+app.put('/api/restaurants/:id',(req,res)=>{
+    const result = validateRestaurant(req.body);
+
+    if(result.error){
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+    updateRestaurant(res, req.params.id, req.body.name, req.body.website, req.body.address, req.body.priceRange, req.body.specialties, req.body.rating, req.body.hours);
+});
+
+async function updateRestaurant(res, id, name, website, address, priceRange, specialties, rating, hours){
+    const result = await Restaurant.updateOne({_id:id},{
+        $set:{
+            name:name,
+            website:website,
+            address:address,
+            priceRange:priceRange,
+            specialties:specialties,
+            rating:rating,
+            hours:hours
+        }
+    })
+
+    res.send(result);
+}
+
+app.delete('/api/restaurants/:id',(req,res)=>{
+    removeRestaurant(res,req.params.id);
+});
+
+async function removeRestaurant(res,id){
+    const restaurant = await Restaurant.findByIdAndRemove(id);
+    res.send(restaurant);
+}
+
+function validateRestaurant(restaurant){
+    const schema = {
+        name:Joi.string().min(3).required(),
+        website:Joi.string().min(3).required(),
+        address:Joi.string().min(3).required(),
+        priceRange:Joi.string().min(3).required(),
+        specialties:Joi.array().items(Joi.string()),
+        rating:Joi.number().min(1).required(),
+        hours:Joi.string().min(3).required()
+    };
+
+    return Joi.validate(restaurant,schema);
+}
+
+app.listen(3000, ()=>{
+    console.log("listening on port 3000");
+})
